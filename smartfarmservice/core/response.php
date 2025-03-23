97826a5e0cfe1c975c4ff5caf7edb64e7fb8dd91
Llt-44 <S6519C10005@sau.ac.th>
2025-03-23 12:05:04 +0700

    Delete smartfarmservice directory

--- smartfarmservice/core/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Response {
-    public static function sendResponse($status, $data) {
-        http_response_code($status);
-        echo json_encode($data);
-        exit;
-    }
-}
-?>
